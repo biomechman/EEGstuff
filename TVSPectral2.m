8555933cbd1ebc02dd4c143db1845e46948da7f2
Gustavo Sandri Heidner <sandriheidnerg17@students.ecu.edu>
2017-08-25 11:40:38 -0400

    0.2

--- TVSPectral2.m
+++ TVSPectral2.m
@@ -6,54 +6,74 @@ channelPath = strcat(uigetdir('Choose the directory cotaining EEG cap config fil
 channels = {'FP1' 'FP2' 'F7' 'F3' 'FZ' 'F4' 'F8' 'C3' 'CZ' 'C4' 'P3' 'P4' 'O1' 'O2'}; % Channels used.
 
 files = dir('*.edf'); % Loads all .edf data files.
-subNum = [1:length(files)]; % Creates array with subject numbers.
-sesNum = 2 % Number of sessions.
+subNum = [1:length(files)]; % Creates array with subject numbers. Subject Numbers are STRINGS!
+sesNum = [1:2]; % Number of sessions. [1 to #]
 typeCond = {'Baseline' 'JessicaJones' 'KillerWomen'}; % Dictionary with the condition type.
 
-for i = 1:length(files)
+fileCount = 0;
+
+for file = files
+    fileCount = fileCount + 1;
+    fileName = file(fileCount).name;
     for j = 1:length(subNum)
-        for k = 1:sesNum
-            filePath = strcat('P',str(j)
+        for k = 1:length(sesNum)
+            for l = 1:length(typeCond)
+                builtName = strcat('P',string(subNum(j)),'_S',string(sesNum(k)),'_',typeCond(l),'_Block');
+                filePath = strcat(dataPath,'/',builtName,'.edf');
+                fprintf(strcat('\n',filePath,'\n\n'))
+
+                % Load and process data set
+                EEG = pop_biosig(dataPath, 'importevent','off','importannot','off');
+                [ALLEEG, EEG, CURRENTSET] = pop_newset(ALLEEG, EEG, 0,'gui','off');
+                EEG = eeg_checkset(pop_eegfiltnew(eeg_checkset(eeg_regepochs(eeg_checkset(EEG))), 4, 35, 1650, 0, [], 0));
+                EEG = pop_chanedit(EEG, 'lookup',filePath);
+                [ALLEEG, EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
+                EEG = eeg_checkset(pop_reref(pop_select(eeg_checkset(EEG),'channel',channels), []));
+                data = EEG.data;
+                
+                for i = 1:length(data(1,1,:))
+                    [spectra,freqs,speccomp,contrib,specstd] = spectopo(data(:,:,i), 0, 1000, 'freqrange',[4 35], 'plot', 'off');
+                    PowerSpec(:,:,i) = spectra; % Power Spectrum - PowerSpec(power at index channel,frequency,epoch)
+                end
+                
+                for i = 1:length(PowerSpec(1,1,:))
+                    for j = 1:length(PowerSpec(:,1,1))
+                        Alpha(j,i) = mean(PowerSpec(j,9:13,i),2); % Band(channel,epochAvg) - e.g.: Alpha(j,i)
+                        Beta(j,i) = mean(PowerSpec(j,16:31,i),2);
+                        Theta(j,i) = mean(PowerSpec(j,7:9,i),2);
+                        Delta(j,i) = mean(PowerSpec(j,2:5,i),2);
+                    end
+                end
+                
+                EngChan = [4, 6, 3, 7]; % Channels used for Engagement, in their proper order. {'FP1' 'FP2' 'F7' 'F3' 'FZ' 'F4' 'F8' 'C3' 'CZ' 'C4' 'P3' 'P4' 'O1' 'O2'}
+                AsymChan = [8, 9, 10, 4, 6, 3, 7, 1, 2, 5, 13, 14, 11, 12]; % Channels used for Asymmetry, in their proper order.
+                
+                fprintf('Creating data matrices.\n')
+                
+                Engagement(:,1:4) = Alpha(EngChan,:)';
+                Engagement(:,5:8) = Beta(EngChan,:)';
+                Engagement(:,9:12) = Theta(EngChan,:)';
+                
+                Asymmetry(:,1:14) = Alpha(AsymChan,:)';
+                Asymmetry(:,15:28) = Beta(AsymChan,:)';
+                Asymmetry(:,29:42) = Theta(AsymChan,:)';
+                Asymmetry(:,43:56) = Delta(AsymChan,:)';
+                
+                if typeCond(l) == typeCond(1) % save Baseline data. Careful here! Baseline must come first and will be assigned to the following condition.
+                    baseline = Engagement;
+                elseif typeCond(l) == typeCond(2) % Write to Jessica Jones .xls file.
+                    writePath = strcat(dataPath,'/P',string(subNum(j)),'_S',string(sesNum(k)),'_JJ_Engagement.xls');
+                    xlswrite(writePath,baseline,1,'K4');
+                    xlswrite(writePath,Engagement,1,'Z4');
+                else % Write to Killer Women .xls file.
+                    writePath = strcat(dataPath,'/P',string(subNum(j)),'_S',string(sesNum(k)),'_KW_Engagement.xls');
+                    xlswrite(writePath,baseline,1,'K4');
+                    xlswrite(writePath,Engagement,1,'Z4');
+                end
+                
+                xlswrite('/home/biomechman/Documents/data/test.xls',Engagement,1,'Z4') %Fix, comment, add another, check
+
+            end
         end
     end
 end
-
-% Load and process data set
-EEG = pop_biosig(dataPath, 'importevent','off','importannot','off');
-[ALLEEG, EEG, CURRENTSET] = pop_newset(ALLEEG, EEG, 0,'gui','off');
-EEG = eeg_checkset(pop_eegfiltnew(eeg_checkset(eeg_regepochs(eeg_checkset(EEG))), 4, 35, 1650, 0, [], 0));
-EEG = pop_chanedit(EEG, 'lookup',filePath);
-[ALLEEG, EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
-EEG = eeg_checkset(pop_reref(pop_select(eeg_checkset(EEG),'channel',channels), []));
-data = EEG.data;
-
-for i = 1:length(data(1,1,:))
-    [spectra,freqs,speccomp,contrib,specstd] = spectopo(data(:,:,i), 0, 1000, 'freqrange',[4 35], 'plot', 'off');
-    PowerSpec(:,:,i) = spectra; % Power Spectrum - PowerSpec(power at index channel,frequency,epoch)
-end
-
-for i = 1:length(PowerSpec(1,1,:))
-    for j = 1:length(PowerSpec(:,1,1))
-        Alpha(j,i) = mean(PowerSpec(j,9:13,i),2); % Band(channel,epochAvg) - e.g.: Alpha(j,i)
-        Beta(j,i) = mean(PowerSpec(j,16:31,i),2);
-        Theta(j,i) = mean(PowerSpec(j,7:9,i),2);
-        Delta(j,i) = mean(PowerSpec(j,2:5,i),2);
-    end
-end
-
-EngChan = [4, 6, 3, 7]; % Channels used for Engagement, in their proper order. {'FP1' 'FP2' 'F7' 'F3' 'FZ' 'F4' 'F8' 'C3' 'CZ' 'C4' 'P3' 'P4' 'O1' 'O2'}
-AsymChan = [8, 9, 10, 4, 6, 3, 7, 1, 2, 5, 13, 14, 11, 12]; % Channels used for Asymmetry, in their proper order.
-
-fprintf('Creating data matrices.\n')
-
-Engagement(:,1:4) = Alpha(EngChan,:)';
-Engagement(:,5:8) = Beta(EngChan,:)';
-Engagement(:,9:12) = Theta(EngChan,:)';
-
-Asymmetry(:,1:14) = Alpha(AsymChan,:)';
-Asymmetry(:,15:28) = Beta(AsymChan,:)';
-Asymmetry(:,29:42) = Theta(AsymChan,:)';
-Asymmetry(:,43:56) = Delta(AsymChan,:)';
-
-
-xlswrite('/home/biomechman/Documents/data/test.xls',Engagement,1,'Z4') 
